a68a46ce1e5ba99c7ba9e9718f2d8de4b0054b5f
= <=>
2024-01-28 19:14:27 +0500

    reset issues

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use("/", (req, res) => {
+  res.status(200).json({
+    msg: "hello world !",
+    success: true,
+  });
+});
+
+app.use("/test", (req, res) => {
+  res.status(200).json({
+    msg: "hello world testing !",
+    success: true,
+  });
+});
+
 app.listen(port, (err) => {
   if (err) console.log("error is ", err);
   console.log("server is listning on 3000 port for Production Environment !");
