2bb946d037386e7b0ed1ce20e76b1c16e2822017
= <=>
2024-01-28 19:06:22 +0500

    error issue resolved

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.listen(port, (errr) => {
+app.listen(port, (err) => {
   if (err) console.log("error is ", err);
   console.log("server is listning on 3000 port ");
 });
